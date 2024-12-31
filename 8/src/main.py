caf0dc1244d4940edaed764ee22a2cc138d8e4c4
Максим <160880502+elow1111@users.noreply.github.com>
2024-12-31 19:33:12 +0300

    main.py

--- 8/src/main.py
+++ 8/src/main.py
@@ -10,5 +10,29 @@ product_list = []
 product_id_counter = 1
 
 # BEGIN (write your solution here)
+class ProductSpecifications(BaseModel):
+    size: str
+    color: str
+    material: str
 
+class ProductBase(BaseModel):
+    name: str
+    price: float = Field(..., gt=0)
+    specifications: ProductSpecifications
+
+class ProductInDB(ProductBase):
+    id: int
+
+@app.post("/product", response_model=ProductInDB)
+async def create_product(product: ProductBase):
+    global product_id_counter
+    product_data = product.dict()
+    product_data["id"] = product_id_counter
+    product_list[product_id_counter] = product_data
+    product_id_counter += 1
+    return product_data
+
+@app.get("/products", response_model=List[ProductInDB])
+async def get_products():
+    return list(product_list.values())
 # END
