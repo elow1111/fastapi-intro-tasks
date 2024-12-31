4bc0d17b5de7d6fc525b920a496f7580f66ebbf7
Максим <160880502+elow1111@users.noreply.github.com>
2024-12-31 19:34:24 +0300

    main.py

--- 9/src/main.py
+++ 9/src/main.py
@@ -10,5 +10,47 @@ product_list = []
 product_id_counter = 1
 
 # BEGIN (write your solution here)
+class ProductSpecifications(BaseModel):
+    size: str
+    color: str
+    material: str
 
+class Product(BaseModel):
+    name: str
+    price: float = Field(..., gt=0)
+    specifications: ProductSpecifications
+
+class ProductResponse(BaseModel):
+    id: int
+    name: str
+    price: float
+
+class ProductDetailResponse(BaseModel):
+    id: int
+    name: str
+    price: float
+    specifications: ProductSpecifications
+
+@app.post("/product", response_model=ProductResponse)
+async def create_product(product: Product):
+    global product_id_counter
+    product_data = product.dict()
+    product_data["id"] = product_id_counter
+    product_list[product_id_counter] = product_data
+    product_id_counter += 1
+    return product_data
+
+@app.get("/products", response_model=List[ProductResponse])
+async def get_products():
+    return [
+        {"id": product_id, "name": product["name"], "price": product["price"]}
+        for product_id, product in product_list.items()
+    ]
+
+@app.get("/product/{product_id}", response_model=ProductDetailResponse)
+async def get_product(product_id: int):
+    product = product_list.get(product_id)
+    if not product:
+        raise HTTPException(status_code=404, detail="Product not found")
+    return product
 # END
