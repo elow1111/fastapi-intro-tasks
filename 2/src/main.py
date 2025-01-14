e4ea0328e5c48007239efd675f1be7998e1584c0
Максим <160880502+elow1111@users.noreply.github.com>
2025-01-14 13:39:51 +0300

    main.py

--- 2/src/main.py
+++ 2/src/main.py
@@ -1,12 +1,12 @@
 from fastapi import FastAPI, Query
-
 app = FastAPI()
 
 # BEGIN (write your solution here)
+from fastapi import HTTPException
 @app.get("/filter")
-async def filter_values(
-    min: int = Query(0, ge=0),
-    max: int = Query(100, le=100)
-):
+async def get_min_and_max(min=0, max=100):
+    min, max = [int(min), int(max)]
+    if min < 0 or max > 100:
+        raise HTTPException(status_code=422, detail="Недопустимое значение минимального или мачимального числа")
     return {"min": min, "max": max}
 # END
