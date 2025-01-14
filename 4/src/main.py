335a2efb8d87581665f2550578af429d19c22149
Максим <160880502+elow1111@users.noreply.github.com>
2025-01-14 13:40:30 +0300

    main.py

--- 4/src/main.py
+++ 4/src/main.py
@@ -4,8 +4,8 @@ app = FastAPI()
 
 # BEGIN (write your solution here)
 @app.get("/language")
-async def get_language(language: str = Cookie(None)):
-    if language is None:
-        return {"message": "Language not set"}
-    return {"language": language}
+async def get_language(language = Cookie(default=None)):
+    if language:
+        return {"language": language}
+    return {'message': "Language not set"}
 # END
