31b485a8694e43e4608fda2b5a8e8088ba2a350b
Максим <160880502+elow1111@users.noreply.github.com>
2025-01-14 13:39:21 +0300

    main.py

--- 1/src/main.py
+++ 1/src/main.py
@@ -4,7 +4,6 @@ app = FastAPI()
 
 # BEGIN (write your solution here)
 @app.get("/reverse/{text}")
-async def reverse_text(text: str):
-    reversed_text = text[::-1]
-    return {"reversed": reversed_text}
+async def get_all_tasks(text):
+    return {"reversed": text[::-1]}
 # END
