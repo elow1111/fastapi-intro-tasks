931e5e454bd2ebd321c2b89f52cf0ee07579d6f0
Максим <160880502+elow1111@users.noreply.github.com>
2025-01-14 13:40:10 +0300

    main.py

--- 3/src/main.py
+++ 3/src/main.py
@@ -3,7 +3,11 @@ from fastapi import FastAPI, Path
 app = FastAPI()
 
 # BEGIN (write your solution here)
+from fastapi import HTTPException
 @app.get("/users/{user_id}")
-async def get_user(user_id: int = Path(..., gt=0)):
+async def get_user_id(user_id):
+    user_id = int(user_id)
+    if user_id<=0:
+        raise HTTPException(status_code=422, detail="Недопустимое значение числа")
     return {"user_id": user_id}
 # END
